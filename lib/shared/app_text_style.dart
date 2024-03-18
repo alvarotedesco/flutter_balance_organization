c3969a89d1ba51d6b325202e8f56c2c9c9f47e64
Thiago Rizzo <rizzo.17@outlook.com>
2024-03-18 00:04:54 -0300

    Feat: created AppTextStyle class to normalize the text styles.

---
+++ lib/shared/app_text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyle {
+  static const TextStyle h3B = TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+}
